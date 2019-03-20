dd724cdde92a6c906d24dfe3ad2c9fcf69ae4773
tomdotscott <tom.scott45456@gmail.com>
2019-03-20 12:31:46 +0000

    Initial commit

---
+++ app/src/main/java/com/tomscott/catgallery/ImageAdapter.java
@@ -0,0 +1,49 @@
+package com.tomscott.catgallery;
+
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+
+    private Context context;
+
+    public Integer[] images = {
+            R.drawable.image1, R.drawable.image2, R.drawable.image3, R.drawable.image4, R.drawable.image5, R.drawable.image6, R.drawable.image7,
+            R.drawable.image8, R.drawable.image9, R.drawable.image10, R.drawable.image11, R.drawable.image12, R.drawable.image13, R.drawable.image14,
+            R.drawable.image15, /*R.drawable.image16, R.drawable.image17, R.drawable.image18, R.drawable.image19, R.drawable.image20
+    */
+    };
+
+    public ImageAdapter(Context c) {
+        context = c;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return images[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = new ImageView(context);
+        imageView.setImageResource(images[position]);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+        imageView.setLayoutParams(new GridView.LayoutParams(240, 240));
+        return imageView;
+    }
+}
